178fc2925f52b8a1e7c2543e83feb14b9837055c
walter <walterrjr.86@gmail.com>
2023-01-23 16:30:12 -0300

    atualizando

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* Route::view('/welcome', 'welcome'); */
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/user/{id}/{name}', function($id = null, $name = null) {
+    return 'User ' . $id . ' - ' . $name;
+})->where([
+    'id' => '[0-9]+',
+    'name' => '[A-Za-z]+'
+]);
+
+/* Route::get('/users', function() {
+    return 'Hello World';
+}); */
+
+/* Route::get('rota-b', function() {
+    return "você está na rota-b";
+}); */
+
+
